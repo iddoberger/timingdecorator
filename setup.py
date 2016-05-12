bdf90ac1d6bfbf7460a0f56dd7d06c59b077a13d
Iddo Berger <iddoberger@gmail.com>
2016-05-12 22:05:21 +0300

    0.1.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'timingdecorator',
   packages = ['timingdecorator'],
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'A timing decorator',
   author = 'Iddo Berger',
   author_email = 'iddoberger@gmail.com',
   url = 'https://github.com/iddoberger/timingdecorator',
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1.2',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1.3',
   keywords = ['timing',], # arbitrary keywords
   classifiers = [],
 )
